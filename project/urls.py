75272da6dbaa6c8648811f6a6bd085f939df59e2
Dana Huffman <dana.huffman@yahoo.com>
2022-09-14 15:41:34 -0700

    Added import for view

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
